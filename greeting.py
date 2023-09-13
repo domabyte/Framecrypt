3fb48f5e8432e1af32252e98f9d5302c4a338eeb
Gyutaro <80272391+Doma-byte@users.noreply.github.com>
2023-09-13 23:22:09 +0530

    Add greeting + import greeting in main + update library version for stability

---
+++ greeting.py
@@ -0,0 +1,32 @@
+import time
+
+
+def print_colored(text, color_code):
+    print(f"\033[{color_code}m{text}\033[0m")
+
+
+def display_intro():
+    logo = f"""
+\033[32m _________   
+|  _______|
+| |_____     
+|  _____|  _____  ____     ____ ___    ___    ____     _____  __  __   ____    __ 
+| |       / ___/ / __ `   / __ `__ \  / _ \  /  __|   / ___/ / / / /  / __ \  / /_
+| |      / /    / /_/ /  / / / / / / /  __/ |  |     / /    / /_/ /  / /_/ / / __/
+|_|     /_/     \__,_/| /_/ /_/ /_/  \___/   \____| /_/     \__, /  / .___/ / / 
+                                                           /____/  /_/      \__|
+ [Dikshit Singh]\033[0m
+    """
+
+    lines = logo.split("\n")
+
+    for line in lines:
+        print_colored(line, "32")
+        time.sleep(0.1)
+
+    print_colored("   Loading...", "33")
+    print_colored("   v1.0.0", "33")
+
+
+if __name__ == "__main__":
+    display_intro()
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from error_handling import prompt_user
 from create_password_protected_zip import create_password_protected_zip
 from decode_frames import decode_frames
+from greeting import display_intro
 import os
 
 def main():
+    display_intro()
     print("What would you like to do?")
     print("[1] Encode a file")
     print("[2] Decode a file")
