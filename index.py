01b6b1718603aede90559a009da9050ab3afda9c
Dikshit singh <singhdikshit69@gmail.com>
2023-09-13 21:09:46 +0530

    Add naming

--- index.py
+++ index.py
@@ -6,8 +6,7 @@ import pyminizip
 import zipfile
 from tqdm import tqdm
 
-
-def error_handling(prompt, validator):
+def prompt_user(prompt, validator):
     while True:
         try:
             user_input = input(prompt).strip("'\"")
@@ -16,141 +15,132 @@ def error_handling(prompt, validator):
             else:
                 print("Invalid input. Please try again.")
         except KeyboardInterrupt:
-            print("Oops! Strong keyboard stroke detected.")
+            print("User interrupted the process.")
             raise Exception("Thanks for using this tool!")
 
-
-def create_password_protected_zip(file_name, password):
+def create_password_protected_zip(input_file, password):
     try:
-        pyminizip.compress(file_name, None, f"locked.zip", password, 5)
-        print("Password-protected ZIP file created successfully!")
+        pyminizip.compress(input_file, None, "locked.zip", password, 5)
+        print("Password-protected ZIP file successfully created!")
         encode_to_frames("locked.zip")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-
 
 def extract_password_protected_zip(zip_file_name, password):
     try:
         with zipfile.ZipFile(zip_file_name, "r") as zip_file:
             zip_file.extractall(pwd=password.encode())
-        print("Password-protected ZIP file extracted successfully!")
+        print("Password-protected ZIP file successfully extracted!")
         os.remove(zip_file_name)
     except zipfile.BadZipFile as e:
-        print(
-            f"Error: The ZIP file is corrupted or not in the correct format: {str(e)}"
-        )
+        print(f"Error: The ZIP file is corrupted or not in the correct format: {str(e)}")
     except RuntimeError as e:
         print(f"Error: Incorrect password or unable to decrypt the ZIP file: {str(e)}")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
-
 def encode_to_frames(input_file):
-    # Checking if encoded file exists
-    file_name = "encoded_file"
+    # Check if the encoded file exists
+    output_file_name = "encoded_video"
     file_num = 1
-    if os.path.exists(f"{file_name}.mp4") is False:
+    if os.path.exists(f"{output_file_name}.mp4") is False:
         pass
     else:
-        while os.path.exists(f"{file_name}({file_num}).mp4"):
+        while os.path.exists(f"{output_file_name}({file_num}).mp4"):
             file_num += 1
-        file_name = f"{file_name}({file_num})"
+        output_file_name = f"{output_file_name}({file_num})"
 
     # Define Video Size
-    width = 1280
-    height = 720
+    video_width = 1280
+    video_height = 720
 
     # Define Pixel Density
-    density = 8
+    pixel_density = 8
 
     # Define Remaining Variables
-    w_pix = 0
-    h_pix = 0
-    frames = 0
+    current_x = 0
+    current_y = 0
+    frame_count = 0
     video = cv2.VideoWriter(
-        f"{file_name}.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 30, (width, height)
+        f"{output_file_name}.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 30, (video_width, video_height)
     )
-    img = Image.new("1", (width, height), "black")
-    print("Generating frames, please be patient...")
+    img = Image.new("1", (video_width, video_height), "black")
+    print("Generating frames, please wait...")
 
     # Generate Density Info Frame
-    if len(str(density)) == 1:
+    if len(str(pixel_density)) == 1:
         density_binary = "".join(format(byte, "08b") for byte in str(0).encode("utf-8"))
         for a in range(len(density_binary)):
             if density_binary[a] == "1":
                 ImageDraw.Draw(img).rectangle(
-                    (w_pix, h_pix, w_pix + (width / 8) - 1, h_pix + (height / 2) - 1),
-                    fill="white",
-                    outline=None,
-                    width=1,
+                    (current_x, current_y, current_x + (video_width / 8) - 1, current_y + (video_height / 2) - 1),
+                    fill="white", outline=None, width=1
                 )
-            w_pix += width / 8
-        w_pix = 0
-        h_pix += height / 2
-    for a in range(len(str(density))):
+            current_x += video_width / 8
+        current_x = 0
+        current_y += video_height / 2
+
+    for a in range(len(str(pixel_density))):
         density_binary = "".join(
-            format(byte, "08b") for byte in str(density)[a].encode("utf-8")
+            format(byte, "08b") for byte in str(pixel_density)[a].encode("utf-8")
         )
         for b in range(len(density_binary)):
             if str(density_binary)[b] == "1":
                 ImageDraw.Draw(img).rectangle(
-                    (w_pix, h_pix, w_pix + (width / 8) - 1, h_pix + (height / 2) - 1),
-                    fill="white",
-                    outline=None,
-                    width=1,
+                    (current_x, current_y, current_x + (video_width / 8) - 1, current_y + (video_height / 2) - 1),
+                    fill="white", outline=None, width=1
                 )
-            w_pix += width / 8
-        w_pix = 0
-        h_pix += height / 2
+            current_x += video_width / 8
+        current_x = 0
+        current_y += video_height / 2
+
     img.save("cache.png")
     video.write(cv2.imread("cache.png"))
-    img = Image.new("1", (width, height), "black")
-    frames += 1
-    w_pix = 0
-    h_pix = 0
+    img = Image.new("1", (video_width, video_height), "black")
+    frame_count += 1
+    current_x = 0
+    current_y = 0
 
     # Generate and Write Frames
     tic = time.perf_counter()
     with open(input_file, "rb") as f:
         byte_count = os.path.getsize(input_file)
-        with tqdm(total=byte_count, unit="byte", unit_scale=True) as pbar:
+        with tqdm(total=byte_count, unit="byte", unit_scale=True) as progress_bar:
             while byte := f.read(1):
                 binary = "{0:08b}".format(int(hex(byte[0])[2:], 16))
                 for a in range(len(binary)):
                     if binary[a] == "1":
                         ImageDraw.Draw(img).rectangle(
-                            (w_pix, h_pix, w_pix + density - 1, h_pix + density - 1),
-                            fill="white",
-                            outline=None,
-                            width=1,
+                            (current_x, current_y, current_x + pixel_density - 1, current_y + pixel_density - 1),
+                            fill="white", outline=None, width=1
                         )
-                    w_pix += density
-                    if w_pix > width - 1:
-                        w_pix = 0
-                        h_pix += density
-                        if h_pix > height - 1:
-                            w_pix = 0
-                            h_pix = 0
+                    current_x += pixel_density
+                    if current_x > video_width - 1:
+                        current_x = 0
+                        current_y += pixel_density
+                        if current_y > video_height - 1:
+                            current_x = 0
+                            current_y = 0
                             img.save("cache.png")
                             video.write(cv2.imread("cache.png"))
-                            img = Image.new("1", (width, height), "black")
-                            frames += 1
-                pbar.update(1)  # Update the progress bar
+                            img = Image.new("1", (video_width, video_height), "black")
+                            frame_count += 1
+                progress_bar.update(1)  # Update the progress bar
+
         img.save("cache.png")
         video.write(cv2.imread("cache.png"))
         toc = time.perf_counter()
         os.remove("cache.png")
         video.release()
-        frames += 1
-    print("Generated " + str(frames) + f" frames in {toc - tic:0.4f} seconds.")
+        frame_count += 1
+    print(f"Generated {frame_count} frames in {toc - tic:0.4f} seconds.")
     cv2.destroyAllWindows()
     os.remove(input_file)
-
 
 def decode_frames(input_file):
     while not os.path.isfile(input_file):
         print("Oops! File does not exist.")
-        input_file = input("What file should I decode?: ")
+        input_file = input("Enter the file to decode: ")
     cap = cv2.VideoCapture(input_file)
 
     output_file = "decoded_file.zip"
@@ -175,9 +165,7 @@ def decode_frames(input_file):
             elif color[0] < 128:
                 binary += "0"
     density = int(
-        bytes(int(binary[i : i + 8], 2) for i in range(0, len(binary), 8)).decode(
-            "utf-8"
-        )
+        bytes(int(binary[i : i + 8], 2) for i in range(0, len(binary), 8)).decode("utf-8")
     )
     binary = ""
 
@@ -208,31 +196,29 @@ def decode_frames(input_file):
                     binary = ""
     os.remove("cache.png")
     file.close()
-    print("Successfully! Decoded the file.")
+    print("Decoding completed successfully.")
     os.remove(input_file)
-    password = str(input("Enter password to extract : "))
+    password = str(input("Enter the password to extract: "))
     extract_password_protected_zip(output_file, password)
-
 
 def main():
     print("What would you like to do?")
-    print("[1] ENCODE a file")
-    print("[2] DECODE a file")
+    print("[1] Encode a file")
+    print("[2] Decode a file")
 
-    user_choice = error_handling("Enter your choice (1/2): ", lambda x: x in ["1", "2"])
+    user_choice = prompt_user("Enter your choice (1/2): ", lambda x: x in ["1", "2"])
 
     if user_choice == "1":
-        input_file = error_handling("Enter the file to encode: ", os.path.isfile)
+        input_file = prompt_user("Enter the file to encode: ", os.path.isfile)
         password = input("Enter a password to protect the ZIP file: ")
         create_password_protected_zip(input_file, password)
     elif user_choice == "2":
-        input_file = error_handling(
-            "Enter the file to decode (i.e, encoded_file.mp4): ", os.path.isfile
+        input_file = prompt_user(
+            "Enter the file to decode (e.g., encoded_file.mp4): ", os.path.isfile
         )
         decode_frames(input_file)
     else:
         print("Invalid choice. Exiting.")
-
 
 if __name__ == "__main__":
     main()
