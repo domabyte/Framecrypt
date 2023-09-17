9c8fa7f85eff1e2439bcf34f6dec45252cee81c8
Dikshit singh <singhdikshit69@gmail.com>
2023-09-17 22:01:43 +0530

    change directory name to Youtube-Upload

--- upload.py
+++ upload.py
@@ -5,6 +5,6 @@ def upload_video_to_youtube():
         title = str(input("Enter the title of the video : "))
         description = str(input("Enter the description of the video : "))
         tags = str(input("Enter tags (comma-separated i.e, boring,pakau) : "))
-        subprocess.run(["node","./Youtube-Uploader/youtube_auth.js",title,description,tags], check=True)
+        subprocess.run(["node","./Youtube-Upload/youtube_auth.js",title,description,tags], check=True)
     except subprocess.CalledProcessError as e:
         print(f'Error uploading video: {e}')
