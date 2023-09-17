e5c85338bc5a964045d25c72e93e8597438c9cfa
Dikshit singh <singhdikshit69@gmail.com>
2023-09-17 21:39:22 +0530

    Running the subprocess for upload videos

---
+++ upload.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def upload_video_to_youtube():
+    try:
+        title = str(input("Enter the title of the video : "))
+        description = str(input("Enter the description of the video : "))
+        tags = str(input("Enter tags (comma-separated i.e, boring,pakau) : "))
+        subprocess.run(["node","./Youtube-Uploader/youtube_auth.js",title,description,tags], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f'Error uploading video: {e}')
