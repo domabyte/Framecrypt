575bac077f95429dd45ed6644e71728afae955ec
Dikshit singh <singhdikshit69@gmail.com>
2023-09-19 09:37:59 +0530

    refactor(youtube_auth.js): remove unused code for video download functionality
    The code in the `listAndDownloadVideos` function was removed as it was no longer needed. The code was responsible for listing videos and prompting the user to choose a video to download. The functionality for downloading videos was also removed, including the `downloadPrivateVideo` function. These changes were made to simplify the code and remove unnecessary functionality.

--- Youtube-Upload/youtube_auth.js
+++ Youtube-Upload/youtube_auth.js
@@ -136,87 +136,11 @@ function listAndDownloadVideos(auth) {
               videos.forEach((video, index) => {
                 console.log(`${index + 1}. ${video.snippet.title}`);
                 console.log(`   Video ID: ${video.snippet.resourceId.videoId}`);
-                const downloadOption = `Download (Press ${index + 1})`;
-                console.log(`   ${downloadOption}`);
-              });
-
-              // Prompt the user for video download choice
-              const rl = readline.createInterface({
-                input: process.stdin,
-                output: process.stdout,
-              });
-              rl.question("Enter the number of the video to download: ", (choice) => {
-                rl.close();
-                const videoIndex = parseInt(choice) - 1;
-                if (isNaN(videoIndex) || videoIndex < 0 || videoIndex >= videos.length) {
-                  console.log("Invalid choice. Please enter a valid number.");
-                } else {
-                  const selectedVideo = videos[videoIndex];
-                  const videoId = selectedVideo.snippet.resourceId.videoId;
-
-                  // Call the download function with videoId and downloadDir
-                  downloadPrivateVideo(auth, `https://www.youtube.com/watch?v=${videoId}`);
-                }
               });
             }
           }
         );
       }
-    }
-  );
-}
-
-function downloadPrivateVideo(auth, videoUrl) {
-  if (!videoUrl) {
-    console.error("Invalid video URL.");
-    return;
-  }
-  const downloadDir = path.join(__dirname,"downloads");
-
-  const videoId = ytdl.getURLVideoID(videoUrl);
-  const videoFilePath = path.join(downloadDir, `${videoId}.mp4`);
-
-  // Use the provided auth object for authorization
-  const oauth2Client = auth;
-
-  // Create a YouTube Data API client
-  const service = google.youtube("v3");
-
-  // Check if the video is private
-  service.videos.list(
-    {
-      auth: oauth2Client,
-      part: "status",
-      id: videoId,
-    },
-    function (err, response) {
-      if (err) {
-        console.error("Error checking video status:", err);
-        return;
-      }
-
-      if (response.data.items.length === 0) {
-        console.error("Video not found.");
-        return;
-      }
-
-      const videoStatus = response.data.items[0].status;
-      if (videoStatus.privacyStatus === "private") {
-        console.error("This is a private video. You do not have access to it.");
-        return;
-      }
-
-      // The video is not private, proceed with downloading
-      const videoStream = ytdl(videoUrl, { quality: "highest" });
-
-      videoStream
-        .pipe(fs.createWriteStream(videoFilePath))
-        .on("finish", () => {
-          console.log(`Video downloaded and saved to: ${videoFilePath}`);
-        })
-        .on("error", (error) => {
-          console.error("Error downloading video:", error);
-        });
     }
   );
 }
