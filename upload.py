b87ea5f708a08dcea3a9ac3936b705d92d09611d
Dikshit singh <singhdikshit69@gmail.com>
2023-09-19 14:44:30 +0530

    fix(upload.py): fix variable name typo in upload_video_to_youtube function
    The variable `videoName` was misspelled as `videoName` in the `upload_video_to_youtube` function. This commit fixes the typo to ensure the correct variable name is used.

--- upload.py
+++ upload.py
@@ -2,9 +2,9 @@ import subprocess
 
 def upload_video_to_youtube():
     try:
+        videoName = str(input("Enter the video name i.e, encoded_video.mp4 : "))
         title = str(input("Enter the title of the video : "))
         description = str(input("Enter the description of the video : "))
-        tags = str(input("Enter tags (comma-separated i.e, boring,pakau) : "))
-        subprocess.run(["node","./Youtube-Upload/youtube_auth.js",title,description,tags], check=True)
+        subprocess.run(["node","./Youtube-Upload/youtube_auth.js",videoName,title,description], check=True)
     except subprocess.CalledProcessError as e:
         print(f'Error uploading video: {e}')
