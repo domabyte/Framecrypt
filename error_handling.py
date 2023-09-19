4382f04552d757cd783a9b4b40c2e937fd6cab8c
Dikshit singh <singhdikshit69@gmail.com>
2023-09-19 14:45:10 +0530

    fix(error_handling.py): add support for handling KeyboardInterrupt and SystemExit exceptions to gracefully exit the program feat(error_handling.py): add new function prompt_user_file to prompt user for a file and validate its existence in the 'encoded_videos' directory

--- error_handling.py
+++ error_handling.py
@@ -1,3 +1,5 @@
+import os 
+
 #global error_handling 
 def prompt_user(prompt, validator):
     while True:
@@ -6,7 +8,24 @@ def prompt_user(prompt, validator):
             if validator(user_input):
                 return user_input
             else:
-                print("Invalid input. Please try again.")
+                print("Invalid input. Please try again.\n")
         except KeyboardInterrupt:
             print("User interrupted the process.")
-            raise Exception("Thanks for using this tool!")
+            raise SystemExit("Thanks for using this tool!")
+        except SystemExit:
+            pass
+
+def prompt_user_file(prompt,validator):
+    while True:
+        try:
+            user_input = input(prompt).strip("'\"")
+            dir_file = os.path.join("encoded_videos",user_input)
+            if validator(dir_file):
+                return user_input
+            else:
+                print("Invalid input. Please try again.\n")
+        except KeyboardInterrupt:
+            print("User interrupted the process.")
+            raise SystemExit("Thanks for using this tool!")
+        except SystemExit:
+            pass
