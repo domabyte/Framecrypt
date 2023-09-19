7cfbad2be80dbbb67d8fe6d168d6d38b31b20764
Dikshit singh <singhdikshit69@gmail.com>
2023-09-19 09:39:07 +0530

    chore(download.py): remove unused download_yt_video function
    The download.py file contained a function called download_yt_video that was not being used. This function was removed to improve code cleanliness and remove unnecessary code.

--- download.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def download_yt_video():
-   try:
-      subprocess.run(["node","./Youtube-Upload/youtube_auth.js", "download"], check=True)
-   except subprocess.CalledProcessError as e:
-      print(f'Error download the video : {e}') 
